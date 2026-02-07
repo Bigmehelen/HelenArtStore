ed91e4a8452c3177bed53fe99cb4066775422dbc
Helen <getfitbyhelena@gmail.com>
2026-02-07 07:41:26 +0100

    Initial commit

---
+++ src/main/java/com/helenartstore/HelenArtStore/HelenArtStoreApplication.java
@@ -0,0 +1,13 @@
+package com.helenartstore.HelenArtStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelenArtStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelenArtStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helenartstore/HelenArtStore/HelenArtStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helenartstore.HelenArtStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelenArtStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
