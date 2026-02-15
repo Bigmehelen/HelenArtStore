6c8f91300fdb01a549209e9db229395dfb2c3d16
Helen <getfitbyhelena@gmail.com>
2026-02-15 03:04:53 +0100

    cloudinary config updated

--- src/main/java/com/helenartstore/HelenArtStore/dtos/response/ArtworkResponse.java
+++ src/main/java/com/helenartstore/HelenArtStore/dtos/response/ArtworkResponse.java
@@ -3,7 +3,10 @@ package com.helenartstore.HelenArtStore.dtos.response;
 import com.helenartstore.HelenArtStore.data.models.User;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.math.BigDecimal;
+import java.util.List;
 
 @Data
 public class ArtworkResponse {
@@ -12,7 +15,7 @@ public class ArtworkResponse {
     private String name;
     private String description;
     private int quantity;
-    private String imageUrls;
+    private List<MultipartFile> images;
     private BigDecimal price;
     private boolean isAvailable;
     private User Artist;
