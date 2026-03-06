00a4086eda7f0c6b8d4151e4c7a6ef7bfa91a10c
Helen <getfitbyhelena@gmail.com>
2026-03-06 09:25:57 +0100

    update

--- src/main/java/com/helenartstore/HelenArtStore/HelenArtStoreApplication.java
+++ src/main/java/com/helenartstore/HelenArtStore/HelenArtStoreApplication.java
@@ -1,13 +1,17 @@
 package com.helenartstore.HelenArtStore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelenArtStoreApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(HelenArtStoreApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+        // Manually load .env and set them as System properties
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(HelenArtStoreApplication.class, args);
+    }
 }
